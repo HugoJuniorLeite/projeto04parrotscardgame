9b2bdddbf4c7a63a53532a87d953966492b70187
[your <[Your>
2022-10-19 22:17:26 -0300

    implementando logica de inicio do jogo

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-alert('teste do link')
+let escolhaDeCartas=Number(prompt("Escolha a quantidade de cartas...O numero deve ser par, maior que 4 e menor que 15"))
+let cartas=document.querySelector('.teste');
+
+while(escolhaDeCartas < 4 || escolhaDeCartas > 15 || escolhaDeCartas% 2 !==0){
+
+        escolhaDeCartas=Number(prompt("Escolha a quantidade de cartas...O numero deve ser par, maior que 4 e menor que 15"));
+                
+    }
+
+    cartas.innerHTML= `liescolhaDeCartas`
+ 
+
+console.log(escolhaDeCartas)
