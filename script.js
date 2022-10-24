2717453165e2fafaec90ccdbc384be0cd914d89a
[your <[Your>
2022-10-24 10:26:29 -0300

    sem o bonus

--- script.js
+++ script.js
@@ -1,50 +1,58 @@
-let escolhaDeCartas
+let escolhaDeCartas;
 let baralho=document.querySelector('.container');
-let cards =[]
-let primeiro =""
-let segundo =""
-let acerto=0
-let erro=0
+const cards =[];
+let primeiro ="";
+let segundo ="";
+let acerto=0;
+let erro=0;
+const personagem=['0','1','2','3','4','5','6'];
 
+
+function embaralhar() { 
+  return Math.random() - 0.5;     
+}
+
+function criarCartas(){
+
+    for(let i =0; i < cards.length; i++){
+
+    baralho.innerHTML += `
+    <div class="card "> 
+    <li id="${cards[i]}" onclick= "selecionarCarta(this)" class="face front"> <img class="image"src="../images/${cards[i]}.gif" alt=""/>  </li>
+    <li id="${cards[i]}" onclick= "selecionarCarta(this)" class="face back"> <img class="image" src="./images/back.png" alt=""/>  </li>
+    </div>
+
+    `;
+
+   //cards.push({card:i}, {personagem: `${i}.gif`});
+}
+
+ 
+}
 
 
 function inicio(){
 
 while(escolhaDeCartas < 4 || escolhaDeCartas > 15 || escolhaDeCartas% 2 !==0){
         escolhaDeCartas=Number(prompt("Escolha a quantidade de cartas...O numero deve ser par, maior que 4 e menor que 15"));
-
         
     }
-
- //   for(let i =0; i < escolhaDeCartas; i++){
-   //     return cards.push([{card :`${i}`, personagem:`${i}.gif`}]) 
-    //}
-
-    criarCartas()
-    }
-
-function criarCartas(){
-
-    for(let i =0; i < escolhaDeCartas/2; i++){
-
-    baralho.innerHTML += `
-    <div class="card ">
-    <li id="${i}" onclick= "selecionarCarta(this)" class="face front"> <img class="image"src="../images/${i}.gif" alt=""/>  </li>
-    <li id="${i}" onclick= "selecionarCarta(this)" class="face back"> <img class="image" src="./images/back.png" alt=""/>  </li>
-    </div>
-    <div class="card">
-    <li id="${i}" onclick= "selecionarCarta(this)" class="face front"> <img class="image"src="../images/${i}.gif" alt=""/> </li>
-    <li id="${i}" onclick= "selecionarCarta(this)" class="face back"> <img class="image" src="./images/back.png" alt=""/> </li>
-    </div>
-    `;
-
-   //cards.push({card:i}, {personagem: `${i}.gif`});
-
-   
-}
+    
+ for(let i =0; i < escolhaDeCartas/2; i++){
+    let card =personagem[i]
+    cards.push(card)
+    cards.push(card) 
+    
+} 
 cards.sort(embaralhar) /*para embaralhar as cartas */
-
+console.log(cards)
+criarCartas()
 }
+console.log (cards)
+
+
+
+
 
 function verificarCards(){
 let firts = primeiro.id
@@ -60,10 +68,6 @@ if(firts === second){
     
 }
 
-
-
-
-
     else {
         
         setTimeout(() =>{
@@ -74,33 +78,23 @@ if(firts === second){
         erro++
     },1000);
 
-
-
-
-
 }
 
 if( acerto === escolhaDeCartas/2 ){
  setTimeout( wins,1000)   
- 
-
 }
 }
-
 
 function wins(){
     const totalJogadas = acerto + erro
    alert(`Você ganhou em ${totalJogadas} jogadas!`)
 }
 
-	
-
 function selecionarCarta(clicou){
  
     if (clicou.parentNode.classList.contains('select-card')) {
         return
     }   
-    
     
     if(primeiro ===""){
     clicou.parentNode.classList.add('select-card')
@@ -113,7 +107,6 @@ else if(segundo===""){
 
     verificarCards()
 }
-
 }
  
     inicio()
@@ -121,11 +114,5 @@ else if(segundo===""){
  // Após esta linha, a minhaArray estará embaralhada
 
 // Esta função pode ficar separada do código acima, onde você preferir
-function embaralhar() { 
-    
-    
-	return Math.random() - 0.5; 
-     
-}
 
 //console.log(cards)
